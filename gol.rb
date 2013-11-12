19e8558158d2a5962b153c3235601e62ac25654a
Josh Cheek <josh.cheek@gmail.com>
2013-11-12 07:35:52 -0600

    Swap some .() for [], At doesn't return EL

--- gol.rb
+++ gol.rb
@@ -22,69 +22,69 @@ Not   = -> condition {
             .(-> { True })}
 
 title "Boolean" #################
-Pass = -> { Print.("\e[32mPass\e[0m") }
-Fail = -> { Print.("\e[31mFAIL\e[0m") }
+Pass = -> { Print["\e[32mPass\e[0m"] }
+Fail = -> { Print["\e[31mFAIL\e[0m"] }
 
-If.(True).(Pass).(Fail)
-If.(False).(Fail).(Pass)
-If.(Not.(True)).(Fail).(Pass)
-If.(Not.(False)).(Pass).(Fail)
+If[True][Pass][Fail]
+If[False][Fail][Pass]
+If[Not[True]][Fail][Pass]
+If[Not[False]][Pass][Fail]
 
 title "Assertions" #################
-AssertEqual = -> a { -> b { If.(Equal.(a).(b)).(Pass).(Fail) } }
-RefuteEqual = -> a { -> b { If.(Equal.(a).(b)).(Fail).(Pass) } }
+AssertEqual = -> a { -> b { If[Equal[a][b]][Pass][Fail] } }
+RefuteEqual = -> a { -> b { If[Equal[a][b]][Fail][Pass] } }
 
-AssertEqual.(1).(1)
-RefuteEqual.(2).(1)
+AssertEqual[1][1]
+RefuteEqual[2][1]
 
 
 title "VALUES" #################
-AssertEqual.(1).(1)
-RefuteEqual.(2).(1)
+AssertEqual[1][1]
+RefuteEqual[2][1]
 
 Cons = -> head {
          -> tail {
            -> f {
-             f.(head).(tail)}}}
+             f[head][tail]}}}
 
 Car = ->list {
-        list.(
-          -> first {
-            -> second {
-              first }})}
+  list[
+    -> first {
+      -> second {
+        first }}]}
 
 Cdr = -> list {
-        list.(-> first {
-          -> second {
-            second }})}
+  list[
+    -> first {
+      -> second {
+        second }}]}
 
 title "LISTS" #################
-MyCons = Cons.(1).(Cons.(2).(nil))
-AssertEqual.(Car.(MyCons)).(1)
-RefuteEqual.(Car.(MyCons)).(2)
+MyCons = Cons[1][Cons[2][nil]]
+AssertEqual[Car[MyCons]][1]
+RefuteEqual[Car[MyCons]][2]
 
-AssertEqual.(Car.(Cdr.(MyCons))).(2)
-RefuteEqual.(Car.(Cdr.(MyCons))).(1)
+AssertEqual[Car[Cdr[MyCons]]][2]
+RefuteEqual[Car[Cdr[MyCons]]][1]
 
 title "CELLS" #################
 
 Cell = -> x {
-         -> y {
-           Cons.(x).(y) }}
+         -> y { Cons[x][y] }}
 
 CellEqual = -> cell1 {
               -> cell2 {
                 # car(cell1) == car(cell2) && cdr(cell1) == cdr(cell2)
-                Equal.(Car.(cell1))
-                     .(Car.(cell2))
-                     .(Equal.(Cdr.(cell1)).(Cdr.(cell2)))
+                Equal.(Car[cell1])
+                     .(Car[cell2])
+                     .(Equal[Cdr[cell1]][Cdr[cell2]])
                      .(False)}}
 
-Cell1 = Cell.(1).(1)
-Cell2 = Cell.(1).(1)
-Cell3 = Cell.(2).(2)
-If.(CellEqual.(Cell1).(Cell2)).(Pass).(Fail)
-If.(Not.(CellEqual.(Cell1).(Cell3))).(Pass).(Fail)
+Cell1 = Cell[1][1]
+Cell2 = Cell[1][1]
+Cell3 = Cell[2][2]
+If[     CellEqual[Cell1][Cell2]  ][Pass][Fail]
+If[ Not[CellEqual[Cell1][Cell3]] ][Pass][Fail]
 
 title "CONSTRUCTING LISTS" #################
 
@@ -92,41 +92,38 @@ EmptyList = -> * { raise "Should not call EmptyList" }
 
 List2 = -> previous {
   -> current {
-    If.(Equal.(current).(EmptyList))
-      .(-> { previous.(EmptyList) })
+    If.(Equal[current][EmptyList])
+      .(-> { previous[EmptyList] })
       .(-> { List2.(
                -> successor {
-                 previous.(Cons.(current).(successor))})})}}
+                 previous[Cons[current][successor]]})})}}
 
 List = -> element {
-  If.(Equal.(element).(EmptyList))
+  If.(Equal[element][EmptyList])
     .(-> { EmptyList })
-    .(-> { List2.(Cons.(element)) })}
+    .(-> { List2[Cons[element]] })}
 
-AssertEqual.(Car.(Cons.(1).(EmptyList))).(1)
-AssertEqual.(List.(EmptyList)).(EmptyList)
-AssertEqual.(Car.(List.(1).(EmptyList))).(1)
-AssertEqual.(Cdr.(List.(1).(EmptyList))).(EmptyList)
-AssertEqual.(Car.(List.(1).(2).(EmptyList))).(1)
-AssertEqual.(Car.(Cdr.(List.(1).(2).(EmptyList)))).(2)
-AssertEqual.(Cdr.(Cdr.(List.(1).(2).(EmptyList)))).(EmptyList)
-AssertEqual.(Car.(Cdr.(Cdr.(List.(1).(2).(3).(EmptyList))))).(3)
+AssertEqual[Car[Cons[1][EmptyList]]][1]
+AssertEqual[List[EmptyList]][EmptyList]
+AssertEqual[Car[List[1][EmptyList]]][1]
+AssertEqual[Cdr[List[1][EmptyList]]][EmptyList]
+AssertEqual[Car[List[1][2][EmptyList]]][1]
+AssertEqual[Car[Cdr[List[1][2][EmptyList]]]][2]
+AssertEqual[Cdr[Cdr[List[1][2][EmptyList]]]][EmptyList]
+AssertEqual[Car[Cdr[Cdr[List[1][2][3][EmptyList]]]]][3]
 
 title "ACCESSING LISTS"
 
 At = -> list {
   -> index {
-    If.(Equal.(index).(0))
-      .(-> { If.(Equal.(list).(EmptyList))
-               .(-> { EmptyList  })
-               .(-> { Car.(list) })})
-      .(-> { At.(Cdr.(list)).(index-1) })}}
+    If.(Equal[index][0])
+      .(-> { Car[list] } )
+      .(-> { At[Cdr[list]][index-1] })}}
 
-list = List.('a').('b').('c').(EmptyList)
-AssertEqual.('a').(At.(list).(0))
-AssertEqual.('b').(At.(list).(1))
-AssertEqual.('c').(At.(list).(2))
-AssertEqual.(EmptyList).(At.(list).(3))
+list = List['a']['b']['c'][EmptyList]
+AssertEqual['a'][At[list][0]]
+AssertEqual['b'][At[list][1]]
+AssertEqual['c'][At[list][2]]
 
 __END__
 AssertEqual.(List.(EmptyList)).(EmptyList)
